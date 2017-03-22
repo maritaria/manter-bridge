b68ad2eeaa0e60e2284b661115cce8470e062426
maritaria <bramkamies@gmail.com>
2017-03-22 10:47:44 +0100

    Implemented dynamic ports

--- manter-module/foo.c
+++ manter-module/foo.c
@@ -1,76 +1,99 @@
 #include <stdio.h>
 
+struct pstate_t {
+	int port;
+	int state;
+};
+
 int read_ports_count = 0;
-int read_ports[1000] = { 0 };
+pstate_t read_ports[1000] = { 0 };
 
 int write_ports_count = 0;
-int write_ports[1000] = { 0 };
+pstate_t write_ports[1000] = { 0 };
 
 void utwente_init(){
 	
 }
+
 void utwente_shutdown(){
 	
 }
 
-void check_read(int port) {
+int check_read(int port, const char* funcname) {
+	pstate_t entry;
 	for(int i = 0; i < read_ports_count; i++) {
-		if (read_ports[i] == port) return;
+		entry = read_ports[i];
+		if (entry.port == port) {
+			printf("utwente read %x %x\n", port, entry.state);
+			return entry.state;
+		}
 	}
-	read_ports[read_ports_count++] = port;
-	printf("utwente read %d\n", port);
+	int state = 0xFF;
+	read_ports[read_ports_count++] = {
+		.port = port,
+		.state = state
+	};
+	printf("utwente read new %x %x\n", port, entry.state);
+	return state;
 }
 
-void check_write(int port) {
+void check_write(int port, int value, const char* funcname) {
+	pstate_t entry;
 	for(int i = 0; i < write_ports_count; i++) {
-		if (write_ports[i] == port) return;
+		entry = write_ports[i];
+		if (entry.port == port) {
+			entry.state = value;
+			printf("utwente write %x %x\n", port, value);
+			return;
+		}
 	}
-	write_ports[write_ports_count++] = port;
-	printf("utwente write %d\n", port);
+	write_ports[write_ports_count++] = {
+		.port = port,
+		.state = value
+	};
+	printf("utwente write new %x %x\n", port, value);
 }
 
 unsigned char inb(unsigned short int port){
-	check_read((int) port);
-	return 0xFF;
-}
-unsigned char inb_p(unsigned short int port){
-	check_read((int) port);
-	return 0xFF;
-}
-unsigned short int inw(unsigned short int port){
-	check_read((int) port);
-	return 0xFF;
-}
-unsigned short int inw_p(unsigned short int port){
-	check_read((int) port);
-	return 0xFF;
-}
-unsigned int inl(unsigned short int port){
-	check_read((int) port);
-	return 0xFF;
-}
-unsigned int inl_p(unsigned short int port){
-	check_read((int) port);
-	return 0xFF;
+	return check_read((int) port, "inb");
 }
 
-void outb(unsigned char value, unsigned short int port){
-	check_write((int) port);
+unsigned char inb_p(unsigned short int port){
+	return check_read((int) port, "inb_p");
 }
+unsigned short int inw(unsigned short int port){
+	return check_read((int) port, "inw");
+}
+unsigned short int inw_p(unsigned short int port){
+	return check_read((int) port, "inw_p");
+}
+unsigned int inl(unsigned short int port){
+	return check_read((int) port, "inl");
+}
+unsigned int inl_p(unsigned short int port){
+	return check_read((int) port, "inl_p");
+}
+
+
+
+void outb(unsigned char value, unsigned short int port){
+	check_write((int) port, value, "outb");
+}
+
 void outb_p(unsigned char value, unsigned short int port){
-	check_write((int) port);
+	check_write((int) port, value, "outb_p");
 }
 void outw(unsigned short int value, unsigned short int port){
-	check_write((int) port);
+	check_write((int) port, value, "outw");
 }
 void outw_p(unsigned short int value, unsigned short int port){
-	check_write((int) port);
+	check_write((int) port, value, "outw_p");
 }
 void outl(unsigned int value, unsigned short int port){
-	check_write((int) port);
+	check_write((int) port, value, "outl");
 }
 void outl_p(unsigned int value, unsigned short int port){
-	check_write((int) port);
+	check_write((int) port, value, "outl_p");
 }
 
 void insb(unsigned short int port, void *addr, unsigned long int count){
