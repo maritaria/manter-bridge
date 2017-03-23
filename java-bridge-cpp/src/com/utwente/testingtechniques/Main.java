69b8ce5e4a3c81d8bf57acd88ec0c643717baa9b
Ivaylo Hristov <i.hristov@zariba.com>
2017-03-23 10:54:05 +0100

    Added bridge code and a sample cpp program.

---
+++ java-bridge-cpp/src/com/utwente/testingtechniques/Main.java
@@ -0,0 +1,93 @@
+package com.utwente.testingtechniques;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+
+        List<String> command = new ArrayList<String>();
+        Scanner s = new Scanner(System.in);
+
+        String currentDir = System.getProperty("user.dir");
+        System.out.println(currentDir);
+        command.add("./src/com/utwente/testingtechniques/cpp_program/sample_cpp_program.exe");
+
+        ProcessBuilder builder = new ProcessBuilder(command);
+        Map<String, String> environ = builder.environment();
+
+        final Process process = builder.start();
+
+        InputStream iutOut = process.getInputStream();
+        InputStream iutErr = process.getErrorStream();
+
+        IUTReader iutReader = new IUTReader(iutOut);
+        iutReader.start();
+
+        ErrReader errReader = new ErrReader(iutErr);
+        errReader.start();
+
+        BufferedWriter pdos = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+
+        for (;;) {
+            pdos.write(Integer.toString(s.nextInt()) + "\n");
+            pdos.flush();
+        }
+    }
+
+    public static class ErrReader extends Thread {
+        private BufferedReader r;
+
+        ErrReader(InputStream is) {
+            this.r = new BufferedReader(new InputStreamReader(is));
+        }
+
+        public void run() {
+            System.out.println("ERRReader running!");
+            String s;
+            while (true) {
+                try {
+                    s = r.readLine();
+                    if (s == null) {
+                        System.err.println("errReader read eof");
+                        return;
+                    }
+                    System.err.println("err: " + s);
+                } catch (Exception e) {
+                    System.err.println("iutReader got exception: " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    public static class IUTReader extends Thread {
+        private BufferedReader r;
+
+        IUTReader(InputStream is) {
+            this.r = new BufferedReader(new InputStreamReader(is));
+        }
+
+        public void run() {
+            System.out.println("IUTReader running!");
+            String b;
+            while (true) {
+                try {
+                    b = r.readLine();
+                    if (b == null) {
+                        break;
+                    }
+                    System.out.println(b);
+                } catch (EOFException e) {
+                    System.err.println("iutReader got eof exception: " + e.getMessage());
+                    return;
+                } catch (Exception e) {
+                    System.err.println("iutReader got exception: " + e.getMessage());
+                }
+            }
+        }
+    }
+}
